796440fa83e835cf7077f3a5d6990e63628dfcd7
hasecom <hasetennisth@gmail.com>
2019-12-24 22:43:27 +0900

    update

---
+++ Views/Scenes/Index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <?php require_once(__DIR__.'/../Components/Header.php'); ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Views/Assets/Css/Index.css">
+    <title>TwiLove</title>
+</head>
+<body>
+    <div id="app">
+        <h2 class="title Anton">Twi-Love</h2>
+        <div class="description">- いいねをしあうWebサービス -</div>
+        <div class="login_btn pointer">
+            <button class="btn btn-lg twitter_btn rounded-pill shadow-lg">Twitterでログインする</button>
+        </div>
+    </div>
+</body>
+<script type="module">
+     var app = new Vue({
+        el: "#app",
+        data: {
+            
+        },
+        mounted(){
+            this.screenFormat();
+        },
+        methods:{
+            screenFormat(){
+                $('#app').width(window.innerWidth).height(window.innerHeight);
+                $('.login_btn').css('top',window.innerHeight/2);
+            }
+        }
+     });
+</script>
+</html>
