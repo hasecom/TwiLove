6769edb8eb523ccbde7bdf916f1a029665a3e44c
hasecom <hasetennisth@gmail.com>
2019-12-29 23:39:57 +0900

    update

--- App/Constants/Domain.php
+++ App/Constants/Domain.php
@@ -3,7 +3,7 @@ namespace App\Constants;
 class Domain{
 
     public static $domain = '/TwiLove';
-    public static $releaseDomain = '/TwiLove';
+    public static $releaseDomain = '/';
     public static function isLocal(){
         return $_SERVER['HTTP_HOST'] == 'localhost';
     }
